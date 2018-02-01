5e596788c9757e244635071fa54be35fb26451f5
sankar112 <35728704+sankar112@users.noreply.github.com>
2018-02-01 16:04:24 +0530

    Create s.c

---
+++ s.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+    int a;
+    printf("enter a letter");
+    scanf("%d",&a);
+    if(a<0)
+    printf("letter is negative");
+    else
+    printf("letter is positive");
+}
