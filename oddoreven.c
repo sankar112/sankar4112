a1b71c4b6917dcf0e7b8c799922d8609b0545e96
sankar112 <35728704+sankar112@users.noreply.github.com>
2018-02-01 16:23:39 +0530

    Create oddoreven.c

---
+++ oddoreven.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+{
+    int N;
+    printf("Enter the number Nu:");
+    scanf("%d",&Nu);
+    if(0<Nu)
+    {
+        printf("Number is positive");
+    }
+         else if(0>Nu)
+         {
+            printf("Number is negative");
+         }
+    else
+    {
+        printf("Number is zero");
+    }
+}
