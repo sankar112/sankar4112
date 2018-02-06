a36bbe5fb7f749e100ae33b55b427672117dff03
sankar112 <35728704+sankar112@users.noreply.github.com>
2018-02-06 16:12:43 +0530

    Create leep r not leep year.c

---
+++ leep r not leep year.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    printf("enter the leep year");
+    scanf("%d",&x);
+if(x%4==0)
+{
+    printf("the leep year");
+}
+else
+{
+    printf(" not leep year");
+    
+}
+}
