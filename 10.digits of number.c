1969d40c0bff7ce09aeb0ee3863c4b9e985439c2
sankar112 <35728704+sankar112@users.noreply.github.com>
2018-02-08 16:43:31 +0530

    Create 10.digits of number.c

---
+++ 10.digits of number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    long long n;
+    int count = 0;
+    clrscr();
+    printf("Enter an integer");
+    scanf("%lld",&n);
+    while(n != 0)
+    {
+        n /= 10;
+        ++count;
+    }
+
+    printf("Number of digits: %d", count);
+    getch();
+    return 0;
+}
+
+
+
+
+
