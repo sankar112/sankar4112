b44e6de63444b8acaff2262d57c6e2c521056d6b
sankar112 <35728704+sankar112@users.noreply.github.com>
2018-02-27 10:54:51 +0000

    Create numeric1.c

---
+++ numeric1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    int i;
+    printf("Enter the string\n");
+    scanf("%d",&i);
+    if(i>0&&i<100000)
+    printf("It is a numeric");
+    else
+    printf("It is not numeric");
+}
