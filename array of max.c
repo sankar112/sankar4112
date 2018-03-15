db4f47ec4a01973efe2f2182cfab32a35794d1eb
sankar112 <35728704+sankar112@users.noreply.github.com>
2018-03-15 03:43:35 -0700

    Create array of max.c

---
+++ array of max.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int a[5],i,j;
+    printf("enter the number:\n");
+    for(i=0;i<5;i++)
+    scanf("%d",&a[i]);
+    printf("enter the number");
+    for(i=0;i<5;i++)
+
+    for(j=i+1;j<5;j++)
+if(a[i]>a[j])
+{
+    int t;
+    t=a[i];
+    a[i]=a[j];
+    a[j]=t;
+}
+for(i=0;i<5;i++)
+printf("%d",a[i]);
+printf("the largest number is%d",a[4]);
+
+}
